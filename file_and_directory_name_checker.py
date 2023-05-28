5bd3a0f18b89a42ef8e9cf96b0de8db4a5de5180
Alex Gregory <alex.gregory1@outlook.com>
2023-05-28 14:59:54 +0100

    Search for invalid directory names using a directory tree

--- file_and_directory_name_checker.py
+++ file_and_directory_name_checker.py
@@ -24,10 +24,44 @@ def get_paths_with_uppercase_in_dirname(paths):
             paths_with_uppercase_in_dirname.append(path)
     return paths_with_uppercase_in_dirname
 
+def add_path_parts_to_directory_tree(path_parts, directory_tree):
+    if path_parts[0] not in directory_tree:
+        directory_tree[path_parts[0]] = {}
+
+    if len(path_parts) == 1:
+        return
+    else:
+        add_path_parts_to_directory_tree(
+            path_parts[1:],
+            directory_tree[path_parts[0]]
+        )
+
+def create_directory_tree(paths):
+    directory_tree = {}
+    for path in paths:
+        if path.is_file():
+            add_path_parts_to_directory_tree(path.parent.parts, directory_tree)
+        else:
+            add_path_parts_to_directory_tree(path.parts, directory_tree)
+    return directory_tree
+
+def directory_name_iter(directory_tree, parents=[], filter_func=lambda _: True):
+    for dir_name, sub_tree in directory_tree.items():
+        if filter_func(dir_name):
+            yield dir_name, parents
+        if sub_tree != {}:
+            yield from directory_name_iter(sub_tree, parents + [dir_name], filter_func)
+
 def main():
     added_filepaths = get_added_filepaths()
     upper_case_filepaths = get_paths_with_uppercase_in_filename(added_filepaths)
-    upper_case_dirpaths = get_paths_with_uppercase_in_dirname(added_filepaths)  
+    directory_tree = create_directory_tree(added_filepaths)
+    upper_case_dirpaths = list(
+        directory_name_iter(
+            directory_tree,
+            filter_func=lambda dir_name: not dir_name.islower()  # type: ignore
+        )
+    )
     ret = 0
     if (len(upper_case_filepaths) != 0) or (len(upper_case_dirpaths) != 0):
         ret = 1
