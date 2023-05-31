c64baeb86a52a86349acadfa8589beb999042632
Alex Gregory <alex.gregory1@outlook.com>
2023-05-31 17:51:05 +0100

    Initialise tests for main.py

---
+++ tests/test_main.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+import pathlib
+
+import pytest
+
+from check_file_and_directory_names import main
+
+@pytest.fixture(scope="session")
+def git_repo_path_no_issues(tmpdir_factory):
+    git_repo_path = tmpdir_factory.mktemp("git_repo_no_issues")
+    git_repo_path = pathlib.Path(git_repo_path)
+    os.chdir(git_repo_path)
+    subprocess.run(["git", "init"])
+    (git_repo_path / "dir").mkdir()
+    (git_repo_path / "dir" / "file1").touch()
+    (git_repo_path / "file2").touch()
+    subprocess.run(["git", "add", "."])
+    return git_repo_path
+
+@pytest.fixture(scope="session")
+def git_repo_path_with_issues(tmpdir_factory):
+    git_repo_path = tmpdir_factory.mktemp("git_repo_with_issues")
+    git_repo_path = pathlib.Path(git_repo_path)
+    os.chdir(git_repo_path)
+    subprocess.run(["git", "init"])
+    (git_repo_path / "Dir").mkdir()
+    (git_repo_path / "Dir" / "file1").touch()
+    (git_repo_path / "File2").touch()
+    subprocess.run(["git", "add", "."])
+    return git_repo_path
+
+def test_main_no_issues(git_repo_path_no_issues):
+    os.chdir(git_repo_path_no_issues)
+    assert main.main() == 0
+
+def test_main_with_issues(git_repo_path_with_issues):
+    os.chdir(git_repo_path_with_issues)
+    assert main.main() == 1
+
