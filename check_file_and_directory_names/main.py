a7dfbd8a2df1d1e75b89537b8af0f5228e51d7ac
Alex Gregory <alex.gregory1@outlook.com>
2023-06-02 20:50:28 +0100

    Add Illegal character check and print flagged files

--- check_file_and_directory_names/main.py
+++ check_file_and_directory_names/main.py
@@ -5,12 +5,14 @@ def main():
     for path in core.get_added_filepaths():
         file_tree.add_path_to_tree(path, path.is_file())
     checkers = [
-        core.CapitalLetterChecker()
+        core.CapitalLetterChecker(),
+        core.IllegalCharacterChecker({"-"})
     ]
     file_tree.run_checkers_over_tree(checkers)
     ret = 0
     for checker in checkers:
         if len(checker.flagged_paths) != 0:
+            core.print_output_from_checkers(checkers)
             ret = 1
     return ret
 
